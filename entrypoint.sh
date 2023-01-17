b59d9e303d1f7a73c6cec66f62affe3d6c9dfe8d
Ron <ronnahom96@gmail.com>
2023-01-17 16:49:12 +0200

    not it work

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,14 +6,17 @@ echo cloning from repo $CLASSIFIED_REPO_URL
 
 whoami
 
-CLASSIFIED_REPO_PATH='/app/classified_assets'
+CLASSIFIED_REPO_PATH='/app/classified_repo'
 cd $CLASSIFIED_REPO_PATH
 
 git clone $CLASSIFIED_REPO_URL .
+echo "Finish clone..."
 
-\cp -r $CLASSIFIED_REPO_PATH/src/lookup-tables/. /app/classified_assets/
+\cp -r $CLASSIFIED_REPO_PATH/src/lookup-tables/. /app/classified_repo/
+echo "Finish first copy..."
 
-\cp -r /app/classified_assets/. /app/dist/assets/
+\cp -r /app/classified_repo/. /app/dist/assets/
+echo "Finish second copy..."
 
 cd /app
 echo "Running start command"
